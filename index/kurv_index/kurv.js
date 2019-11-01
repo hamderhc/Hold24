bfaa8d287b6240f51cef35e64021bdbf5d0c9a42
Jonathan <jomo19ad@student.cbs.dk>
2019-11-01 08:36:39 +0100

    Kurv JS + HTML

--- index/kurv_index/kurv.js
+++ index/kurv_index/kurv.js
@@ -1,3 +1,26 @@
+
+var lineS = document.getElementsByClassName('lineStanders')
+var lineS1 = document.getElementsByClassName('lineStanders1')
+var lineS2 = document.getElementsByClassName('lineStanders2')
+var lineS3 = document.getElementsByClassName('lineStanders3')
+
+class chosenLinestander {
+    constructor (name, location, price) {
+        this.name = name;
+        this.location = location;
+        this.price = price;
+    }
+
+    createHTML() {
+        var addbutton = "<input type='button' class='addToStorage' name='add to storage' data-object='" + JSON.stringify(this) + "' value='Add to Storage' id='click'></input>";
+    }
+
+
+}
+
+
+
+// calculate price function
 function calculatePrice() {
     var inputEl = document.getElementById("antalTimer");
     var inputText = inputEl.value;
