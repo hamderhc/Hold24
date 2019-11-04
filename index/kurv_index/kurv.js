f1872b63d7656683a7b8e533262fa1c572770a1c
Jonathan <jomo19ad@student.cbs.dk>
2019-11-04 14:54:08 +0100

    Kurv JS + HTML_4/11/19

--- index/kurv_index/kurv.js
+++ index/kurv_index/kurv.js
@@ -1,4 +1,68 @@
+// skal slettes - bliver brugt i samme mappe som festival.html
 
+
+
+
+//create array that will hold all ordered products
+var shoppingCart = [];
+
+//this function manipulates DOM and displays content of our shopping cart
+function displayShoppingCart(){
+    var orderedProductsTblBody=document.getElementById("orderedProductsTblBody");
+    //ensure we delete all previously added rows from ordered products table
+    while(orderedProductsTblBody.rows.length>0) {
+        orderedProductsTblBody.deleteRow(0);
+    }
+
+    //variable to hold total price of shopping cart
+    var cart_total_price=0;
+    //iterate over array of objects
+    for(var product in shoppingCart){
+        //add new row
+        var row=orderedProductsTblBody.insertRow();
+        //create three cells for product properties
+        var cellName = row.insertCell(0);
+        var cellDescription = row.insertCell(1);
+        var cellPrice = row.insertCell(2);
+        cellPrice.align="right";
+        //fill cells with values from current product object of our array
+        cellName.innerHTML = shoppingCart[product].Name;
+        cellDescription.innerHTML = shoppingCart[product].Description;
+        cellPrice.innerHTML = shoppingCart[product].Price;
+        cart_total_price+=shoppingCart[product].Price;
+    }
+    //fill total cost of our shopping cart
+    document.getElementById("cart_total").innerHTML=cart_total_price;
+}
+
+
+function AddtoCart(name,description,price){
+    //Below we create JavaScript Object that will hold three properties you have mentioned:    Name,Description and Price
+    var singleProduct = {};
+    //Fill the product object with data
+    singleProduct.Name=name;
+    singleProduct.Description=description;
+    singleProduct.Price=price;
+    //Add newly created product to our shopping cart
+    shoppingCart.push(singleProduct);
+    //call display function to show on screen
+    displayShoppingCart();
+
+}
+
+
+//Add some products to our shopping cart via code or you can create a button with onclick event
+
+//AddtoCart("josef","Århus",120);
+function removeAll(){
+    document.getElementById("orderedProductsTblBody").innerHTML = "";
+    document.getElementById("cart_total").innerHTML = "";
+}
+
+
+
+
+/*
 
 var lineS = document.getElementsByClassName('lineStanders')
 var lineS1 = document.getElementsByClassName('lineStanders1')
@@ -19,6 +83,7 @@ class ChosenLinestander {
     }
  */
 
+/*
 }
 
 var list = [];
